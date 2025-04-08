e8bd100d461b6c531424046b87dc760e31f5ce6d
bradyshehu <shehu.brady@gmail.com>
2025-04-08 15:40:08 +0200

    initials

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,21 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+console.table(names);
 
 // Dichiara la funzione qui.
 
+function initials(haystack) {
+  const firstLetter = [];
+  for (i = 0; i < haystack.length; i++) {
+    const currentItem = haystack[i];
+    firstLetter.push(currentItem[0]);
+  }
+  return firstLetter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+initials(names);
+console.table(initials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
